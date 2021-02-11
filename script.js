fd201510e560b081a4089e3b784c40a5c74a7cbf
Glitch (204671m-googlecopy) <none>
2021-02-11 14:19:01 +0000

    🏄🏕 Updated with Glitch

--- script.js
+++ script.js
@@ -3,3 +3,14 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
